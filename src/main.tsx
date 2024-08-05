94bae86ffec063b5145ee35093c0cef48b784b70
Mateusz Ulazka <m.ulazka@godeltech.com>
2024-08-05 15:51:55 +0200

    remove app file

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 
 import "./main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import store from "./store.ts";
+import { router } from "./router.ts";
 
 document.getElementsByTagName("html")[0].dataset.bsTheme = "dark";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <div>
+        <RouterProvider router={router}></RouterProvider>
+      </div>
+    </Provider>
   </React.StrictMode>
 );
